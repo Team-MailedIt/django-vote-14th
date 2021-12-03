ba8483c44525c359d6f95293fab64fbfe1e4aa91
mongus <kjyanus@gmail.com>
2021-12-03 14:37:22 +0900

    feat: 토큰 쿠키에 저장 및 응답 형식 수정

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterAPIView, CandidateListAPIView, CandidateDetailAPIView, VoteAPIView, TestAPIView
+from .views import *
 
 urlpatterns = [
-    path("signin", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("signin", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     # 세션 연장하고 싶을 때 refresh token 사용
     # path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("signin", AuthView.as_view()),
     path("signup", RegisterAPIView.as_view()),
     path("candidate", CandidateListAPIView.as_view()),
     path("candidate/<int:pk>", CandidateDetailAPIView.as_view()),
